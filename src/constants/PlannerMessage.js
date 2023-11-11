30922623d3f28843674ad51e5e1566c7783335ef
Goonco <s07019@naver.com>
2023-11-11 20:40:47 +0900

    :art: 이벤트 플래너 날짜 선택 메세지, 메뉴 선택 메세지 상수화

--- src/constants/PlannerMessage.js
+++ src/constants/PlannerMessage.js
@@ -3,3 +3,10 @@ export const PRINT_MESSAGE = {
   SHOW_BENEFITS: (visitDate) =>
     `${visitDate}월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!`,
 };
+
+export const READ_MESSAGE = {
+  SELECT_VISIT_DATE:
+    '12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)',
+  SELECT_MENU:
+    '주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)',
+};
