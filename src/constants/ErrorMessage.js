5cc30ba083f86e79eccd99773e99e8c2778a2f74
Goonco <s07019@naver.com>
2023-11-12 15:51:49 +0900

    :art: 메뉴-주문수 입력에 대한 에러 메세지 상수화 & 메뉴 목록 상수화

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -2,3 +2,18 @@ export const ERROR_DATE_NOT_A_INTEGER =
   '[ERROR} 날짜 입력값이 정수가 아닙니다. 다시 입력해 주세요.';
 export const ERROR_DATE_OUT_OF_BOUND =
   '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.';
+
+export const ERROR_MENU_NOT_EXISTING =
+  '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+
+export const ERROR_MENU_NOT_UNIQUE =
+  '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+
+export const ERROR_MENU_ONLY_DRINKS =
+  '[ERROR] 음료 외의 주문이 필요합니다. 다시 입력해 주세요.';
+
+export const ERROR_ORDER_NOT_POSTIVE_INTEGER =
+  '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.';
+
+export const ERROR_ORDER_TOO_MUCH_ORDER =
+  '[ERROR] 메뉴는 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.';
