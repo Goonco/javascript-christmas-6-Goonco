401ee3a3e7f4951bdaed0e4586c2914413e1b887
Goonco <s07019@naver.com>
2023-11-12 18:10:22 +0900

    :sparkles: 주문 내역 메세지 관련 출력 기능 추가
    * 주문 내역 출력 전 12월 N일에 ... 메세지 출력 기능 추가
    * <주문 메뉴> 출력 추가
    * 메뉴-주문수 쌍 출력 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -1,9 +1,29 @@
 import { Console } from '@woowacourse/mission-utils';
-import { PRINT_MESSAGE } from '../constants/PlannerMessage.js';
+import {
+  PRINT_FORMATS,
+  PRINT_HEADERS,
+  PRINT_MESSAGE,
+} from '../constants/PlannerMessage.js';
 
 const OutputView = {
   printStartMessage() {
     Console.print(PRINT_MESSAGE.START_PLANNER);
+  },
+
+  printShowBenefitMessage(visitDate) {
+    Console.print(PRINT_MESSAGE.SHOW_BENEFITS(visitDate));
+  },
+
+  printOrderResult(orderList) {
+    Console.print(PRINT_HEADERS.ORDER_LIST);
+    orderList.forEach((order) => {
+      const [menu, orderQuantatiy] = [...order];
+      Console.print(PRINT_FORMATS.ORDER_RESULT(menu, orderQuantatiy));
+    });
+  },
+
+  printLineChange() {
+    Console.print('\n');
   },
 };
 
