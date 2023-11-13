cb2130aa8cf54204c8939927b1dd178dc52d44da
Goonco <s07019@naver.com>
2023-11-13 17:31:17 +0900

    :white_check_mark: VisitDate 내의 특정 날인지 확인하는 메소드들에 대한 테스트 구현

--- __tests__/VisitDateTest.js
+++ __tests__/VisitDateTest.js
@@ -17,4 +17,40 @@ describe('VisitDate 클래스 테스트', () => {
       new VisitDate(32);
     }).toThrow(ERROR_DATE_OUT_OF_BOUND);
   });
+
+  test('날짜가 일 ~ 목일 경우 true를 반환하며 금 ~ 토일 경우 false를 반환한다.', () => {
+    const input = [5, 13, 21, 29];
+    const log = [true, true, true, false];
+
+    input.forEach((date, idx) => {
+      expect(new VisitDate(date).checkWeekDay()).toBe(log[idx]);
+    });
+  });
+
+  test('날짜가 금 ~ 토일 경우 true를 반환하며 일 ~ 목일 경우 false를 반환한다.', () => {
+    const input = [8, 16, 23, 28];
+    const log = [true, true, true, false];
+
+    input.forEach((date, idx) => {
+      expect(new VisitDate(date).checkWeekendDay()).toBe(log[idx]);
+    });
+  });
+
+  test('일요일 혹은 크리스마스일 경우 true를 반환하며 이외의 경우 false를 반환한다.', () => {
+    const input = [3, 10, 25, 26];
+    const log = [true, true, true, false];
+
+    input.forEach((date, idx) => {
+      expect(new VisitDate(date).checkSpecialDay()).toBe(log[idx]);
+    });
+  });
+
+  test('1일로부터 몇일이 지났는지 반환한다.', () => {
+    const input = [1, 5, 9, 24, 28];
+    const log = [0, 4, 8, 23, -1];
+
+    input.forEach((date, idx) => {
+      expect(new VisitDate(date).checkChristmasDDay()).toBe(log[idx]);
+    });
+  });
 });
