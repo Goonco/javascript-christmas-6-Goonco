cfd39aa02cf19ebf15c7cec2cd4ce3964e0f6193
Goonco <s07019@naver.com>
2023-11-13 23:14:46 +0900

    :white_check_mark: 혜택 목록 작성 테스트 추가 및 기존 증정품 테스트 통합

--- __tests__/BenefitCheckerTest.js
+++ __tests__/BenefitCheckerTest.js
@@ -5,16 +5,19 @@ import DEFAULT_BENEFIT_BOARD from '../src/constants/Benefit.js';
 
 describe('BenefitChecker 클래스 테스트', () => {
   const benefitChecker = new BenefitChecker();
+  const visitDate = new VisitDate(3);
+  const menuOrder = new MenuOrder({
+    menuList: ['티본스테이크', '바비큐립', '초코케이크', '제로콜라'],
+    orderNumberList: [1, 1, 2, 1],
+  });
 
-  test('주문 금액이 120000 이상이면 샴페인을 증정 받는다.', () => {
-    const visitDate = new VisitDate(25);
-    const menuOrder = new MenuOrder({
-      menuList: ['티본스테이크', '바비큐립', '초코케이크', '제로콜라'],
-      orderNumberList: [1, 1, 2, 1],
-    });
+  test('주문에 대한 혜택 목록을 작성한다.', () => {
     benefitChecker.checkAllBenefit(visitDate, menuOrder);
     expect(benefitChecker.getBenefitBoard()).toEqual({
-      ...DEFAULT_BENEFIT_BOARD,
+      christmasDDay: 1200,
+      weekDay: 4046,
+      weekendDay: 0,
+      special: 1000,
       freebie: ['샴페인', 25000],
     });
   });
