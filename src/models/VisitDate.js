b70427797a076b5102e8790bce9977cb19ac7551
Goonco <s07019@naver.com>
2023-11-13 17:28:32 +0900

    :sparkles: 방문 일자가 특정 날인지 확인하는 기능 목록 구현
    * 방문일자가 주말인지 확인하는 기능 구현
    * 방문일자가 주중인지 확인하는 기능 구현
    * 방문일자가 이벤트 달력에 별이 있는 날인지 확인하는 기능 구현
    * 방문일자가 12월 1일부터 몇일 지났는지 확인하는 기능 구현

--- src/models/VisitDate.js
+++ src/models/VisitDate.js
@@ -5,16 +5,42 @@ class VisitDate {
 
   constructor(visitDate) {
     this.#validate(visitDate);
-    this.#visitDate = visitDate;
+    this.#visitDate = new Date(`2023-12-${visitDate}`);
   }
 
   getVisitDate() {
-    return this.#visitDate;
+    return this.#visitDate.getDate();
   }
 
   #validate(visitDate) {
     isInteger(visitDate);
     isDateInBound(visitDate);
+  }
+
+  checkWeekDay() {
+    const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+    const day = DAYS[this.#visitDate.getDay()];
+    if (day !== '금' && day !== '토') return true;
+    return false;
+  }
+
+  checkWeekendDay() {
+    const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+    const day = DAYS[this.#visitDate.getDay()];
+    if (day === '금' || day === '토') return true;
+    return false;
+  }
+
+  checkSpecialDay() {
+    const DAYS = ['일', '월', '화', '수', '목', '금', '토'];
+    const day = DAYS[this.#visitDate.getDay()];
+    if (day === '일' || this.#visitDate.getDate() === 25) return true;
+    return false;
+  }
+
+  checkChristmasDDay() {
+    if (this.#visitDate.getDate() <= 25) return this.#visitDate.getDate() - 1;
+    return -1;
   }
 }
 
