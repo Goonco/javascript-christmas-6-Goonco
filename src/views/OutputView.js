252d5df6ed44af36deba6e853025ac203af2dcf3
Goonco <s07019@naver.com>
2023-11-14 12:55:01 +0900

    :sparkles: 각 혜택 목록 출력 기능 추가
    * 증정품 출력 기능 추가
    * 이벤트 할인 출력 기능 추가
    * 총 혜택 금액 출력 기능 추가
    * 할인 이후 결제 금액 출력 기능 추가
    * 뱃지 출력 기능 추가
    * 에러 메세지 출력 기능 추가

--- src/views/OutputView.js
+++ src/views/OutputView.js
@@ -24,11 +24,68 @@ const OutputView = {
 
   printTotalPriceBeforeSale(totalPrice) {
     Console.print(PRINT_HEADERS.BEFORE_SALE);
-    Console.print(PRINT_FORMATS.BEFORE_SALE_RESULT(totalPrice));
+    Console.print(PRINT_FORMATS.POSITIVE_MONEY_RESULT(totalPrice));
+  },
+
+  printTotalPriceAfterSale(totalPrice) {
+    Console.print(PRINT_HEADERS.AFTER_SALE);
+    Console.print(PRINT_FORMATS.POSITIVE_MONEY_RESULT(totalPrice));
+  },
+
+  printEventBadge(eventBadge) {
+    Console.print(PRINT_HEADERS.BADGE);
+    Console.print(eventBadge);
+  },
+
+  printTotalBenefit(totalBenefitPrice) {
+    Console.print(PRINT_HEADERS.TOTAL_BENEFITS);
+    Console.print(PRINT_FORMATS.NEGATIVE_MONEY_RESULT(totalBenefitPrice));
+  },
+
+  printFreebie(freebie) {
+    Console.print(PRINT_HEADERS.FREEBIE);
+    if (freebie === 0) Console.print(PRINT_FORMATS.NO_BENEFIT);
+    else Console.print(PRINT_FORMATS.ORDER_RESULT('샴페인', 1));
+  },
+
+  printBenefitList(benefitList, totalBenefit) {
+    Console.print(PRINT_HEADERS.BENEFITS);
+    if (totalBenefit === 0) Console.print(PRINT_FORMATS.NO_BENEFIT);
+    this.printChristmasSale(benefitList.christmasDDay);
+    this.printWeekDaySale(benefitList.weekDay);
+    this.printWeekendDaySale(benefitList.weekendDay);
+    this.printSpecialSale(benefitList.special);
+    this.printFreebieSale(benefitList.freebie);
+  },
+
+  printChristmasSale(sale) {
+    if (sale !== 0) Console.print(PRINT_FORMATS.CHRISTMAS_BENEFIT_RESULT(sale));
+  },
+
+  printWeekDaySale(sale) {
+    if (sale !== 0) Console.print(PRINT_FORMATS.WEEKDAY_BENEFIT_RESULT(sale));
+  },
+
+  printWeekendDaySale(sale) {
+    if (sale !== 0)
+      Console.print(PRINT_FORMATS.WEEKENDDAY_BENEFIT_RESULT(sale));
+  },
+
+  printSpecialSale(sale) {
+    if (sale !== 0) Console.print(PRINT_FORMATS.SPECIAL_BENEFIT_RESULT(sale));
+  },
+
+  printFreebieSale(freebie) {
+    if (freebie !== 0)
+      Console.print(PRINT_FORMATS.FREEBIE_BENEFIT_RESULT(freebie[1]));
   },
 
   printLineChange() {
     Console.print('');
+  },
+
+  printErrorMessage(message) {
+    Console.print(`${message}`);
   },
 };
 
