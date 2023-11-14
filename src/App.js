633dcea379ecf36353de20647115b71e0c0d5192
Goonco <s07019@naver.com>
2023-11-14 12:56:14 +0900

    :sparkles: 4개의 소기능 흐름 생성

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import EventPlanner from './EventPlanner.js';
+
 class App {
-  async run() {}
+  async run() {
+    const eventPlanner = new EventPlanner();
+    eventPlanner.activate();
+    const visitDate = await eventPlanner.generateVisitDate();
+    const menuOrder = await eventPlanner.generateMenuOrder();
+    eventPlanner.showOrderResult(visitDate, menuOrder);
+    const benefitChecker = eventPlanner.calculateBenefits(visitDate, menuOrder);
+    eventPlanner.showBenefitResult(menuOrder, benefitChecker);
+  }
 }
 
 export default App;
