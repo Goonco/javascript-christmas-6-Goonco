fdaaf821652900d69080584ec0cfa55bc20c36e1
Goonco <s07019@naver.com>
2023-11-15 02:51:32 +0900

    :recycle: 객체 재분리로인한 상수 재분리

--- src/constants/ErrorMessage.js
+++ src/constants/ErrorMessage.js
@@ -4,16 +4,16 @@ export const ERROR_VISIT_DATE = {
   DATE_OUT_OF_BOUND: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
 };
 
-export const ERROR_MENU = {
+export const ERROR_ORDER = {
+  QUANTITY_NOT_POSITIVE_INTEGER:
+    '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
   MENU_NOT_EXISTING: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
-  MENU_NOT_UNIQUE: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
-  MENU_ONLY_DRINKS: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
 };
 
-export const ERROR_ORDER = {
-  ORDER_NOT_POSITIVE_INTEGER:
-    '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
-  ORDER_TOO_MUCH: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+export const ERROR_ORDERLIST = {
+  MENU_NOT_UNIQUE: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  MENU_ONLY_DRINKS: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  QUANTITY_TOO_MUCH: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
 };
 
 export const ERROR_CALENDAR = {
